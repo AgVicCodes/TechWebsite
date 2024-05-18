8a5ce8edc2d1ffb2ca65d2773a4f4431b94d92ca
AgVicCodes <victoragbeleye2@gmail.com>
2024-05-18 23:37:42 +0100

    Layout templating

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,10 @@
 const Footer = () => {
     return (
-        <>
-            <span>Copyright © 2024</span>
-        </>
+        <footer className="container">
+            <div className="flex">
+                <span className="mx-auto">Copyright © 2024</span>
+            </div>
+        </footer>
     );
 }
  
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,15 +2,15 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <nav>
-            <div className = "logo">
-                <h1 className = "font-bold">ACUMEN</h1>
+        <nav className="container">
+            <div className = "grid">
+                <h1 className = "font-bold mx-auto">ACUMEN</h1>
                 {/* <img src = "" alt = "Logo"/> */}
+                <Link href = "/" className = "m-4">Home</Link>
+                <Link href = "/" className = "m-4">Blog</Link>
+                <Link href = "/about" className = "m-4">About</Link>
+                <Link href = "/" className = "m-4">Contact</Link>
             </div>
-            <Link href = "/" className = "m-4">Home</Link>
-            <Link href = "/" className = "m-4">Blog</Link>
-            <Link href = "/about" className = "m-4">About</Link>
-            <Link href = "/" className = "m-4">Contact</Link>
         </nav>
     );
 }
