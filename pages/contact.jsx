8e7ffd687a39272eb4af7400c8656f21200c2a04
AgVicCodes <victoragbeleye2@gmail.com>
2024-05-21 23:57:59 +0100

    Contact Page Created

---
+++ pages/contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi exercitationem quae, quisquam perspiciatis ipsa voluptatum sunt voluptate aperiam hic maiores error facilis praesentium. Ab ad ullam provident dolorum exercitationem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi exercitationem quae, quisquam perspiciatis ipsa voluptatum sunt voluptate aperiam hic maiores error facilis praesentium. Ab ad ullam provident dolorum exercitationem.</p>
+        </>
+     );
+}
+ 
+export default Contact;
