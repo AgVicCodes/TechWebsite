4e69a85e6e7a5000321d8048800886cc289e7811
AgVicCodes <victoragbeleye2@gmail.com>
2024-05-22 23:48:30 +0100

    Added Lorem text

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,8 +3,9 @@ const Contact = () => {
         <>
             <h1>About Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi exercitationem quae, quisquam perspiciatis ipsa voluptatum sunt voluptate aperiam hic maiores error facilis praesentium. Ab ad ullam provident dolorum exercitationem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi exercitationem quae, quisquam perspiciatis ipsa voluptatum sunt voluptate aperiam hic maiores error facilis praesentium. Ab ad ullam provident dolorum exercitationem.</p>
         </>
-    );
+     );
 }
-
+ 
 export default Contact;
