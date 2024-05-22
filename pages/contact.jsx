f8587ba01590eddab175eb3664143456e60d1451
AgVicCodes <victoragbeleye2@gmail.com>
2024-05-22 23:48:18 +0100

    Removed Lorem text

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,9 +3,8 @@ const Contact = () => {
         <>
             <h1>About Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi exercitationem quae, quisquam perspiciatis ipsa voluptatum sunt voluptate aperiam hic maiores error facilis praesentium. Ab ad ullam provident dolorum exercitationem.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi exercitationem quae, quisquam perspiciatis ipsa voluptatum sunt voluptate aperiam hic maiores error facilis praesentium. Ab ad ullam provident dolorum exercitationem.</p>
         </>
-     );
+    );
 }
- 
+
 export default Contact;
